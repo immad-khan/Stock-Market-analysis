bd169e5e110ed5c5d9db8cf420742fac5c68a46e
immad-khan <immadkhan303@gmail.com>
2026-09-25 23:49:20 +0500

    added companies whose data will be added

---
+++ app/ml/constants.py
@@ -0,0 +1,7 @@
+TICKERS = [
+    "AAPL", "MSFT", "GOOGL", "AMZN", "TSLA",
+    "INFY.NS", "TCS.NS", "RELIANCE.NS", "HDFCBANK.NS",
+]
+
+TRAINING_PERIOD = "5y"
+LOOKBACK_DAYS = 60  # Trading rows, not calendar days
